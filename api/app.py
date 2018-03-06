1b540319df3ae6a86ea870db04abe7f65175071b
SelaDanti <kwameasiago@gmail.com>
2018-03-06 16:13:15 +0300

    set up test for status code and set up a registration class

--- api/app.py
+++ api/app.py
@@ -1,11 +1,39 @@
 from flask import Flask
-from flask_restplus import Api
+from flask_restplus import Api,Resource,fields
+from model import users
+
+
 app=Flask(__name__)
 api=Api(app)
+userObj=users()
+#start of api model
+registerModel=api.model('registerModel',{
+	"firstName":fields.String,
+	"lastName":fields.String,
+	"email":fields.String,
+	"password":fields.String,
+	"confirmPassword":fields.String,
+	})
+#end of api model
 
-@app.route('/')
-def index():
-	return "Flask  and restplus is set up"
+@api.route('/register')
+class register(Resource):
+	@api.expect(registerModel)
+	def post(self):
+		newUser = api.payload
+		email=newUser['email']
+		if newUser['password'] != newUser['confirmPassword']:
+			return {"result":"password do not match"}
+		elif userObj.findin(userObj.users,email)==True
+			return {"result":"email already exist"}
+		else:
+			userObj.users.append(newUser)
+			return {"result":"post added"}
+
+	def get(self):
+		return userObj.users
+		
+
 
 if __name__=='__main__':
 	app.run(debug=True)
