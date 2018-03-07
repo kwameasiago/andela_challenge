6bdfa4e2796bb299225317c03f414e970d9d740f
SelaDanti <kwameasiago@gmail.com>
2018-03-07 22:47:51 +0300

    impliment OOP in data structure

--- api/app.py
+++ api/app.py
@@ -2,12 +2,12 @@ from flask import Flask,request
 import json
 from flask_restplus import Api,Resource,fields
 from  werkzeug.security import generate_password_hash , check_password_hash
-from models import User
+from models import Data_storage,Verification
 
 
 app = Flask(__name__)
 api = Api(app)
-user_obj = User()
+user_obj = Verification()
 
 
 
@@ -15,11 +15,22 @@ user_obj = User()
 register_model = api.model('registerModel',{
 	"first_name" : fields.String,
 	"last_name" : fields.String,
-	"email" : fields.String,
+	"email" : fields.String('kwame@gmail.com'),
 	"password" : fields.String,
 	"confirm_password" : fields.String
+})
+login_model = api.model('login_info',{
+	'email':fields.String,
+	'password':fields.String
+})
+logout_model=api.model('logout_model',{
+	'confirm_logout':fields.String('no')
+})
+password_reset=api.model('password_reset',{
+	'old_password':fields.String,
+	'new_password':fields.String,
+	'confirm_password':fields.String
 	})
-
 
 #user registration endpoint
 @api.route('/api/auth/register')
@@ -37,31 +48,59 @@ class Register(Resource):
 			return {'Error':'email aready exists'}
 		elif user_obj.email_verification(new_user['email']) == False:
 			return {'Error':'Incorrect Email syntax'}
+		elif user_obj.not_empty(new_user['first_name']) == True:
+			return {'Error':'first name can not be empty'}
+		elif user_obj.not_empty(new_user['last_name']) == True:
+			return {'Error':'last name can not be empty'}			
+		elif user_obj.not_empty(new_user['password']) == True:
+			return {'Error':'password can not be empty'}			
 		else:
-			return {'ok':''}
+			new_user['user_id']=len(user_obj.user_data)+1
+			del new_user['confirm_password']
+			user_obj.user_data.append(new_user)
+			new_user['password']=generate_password_hash(new_user['password'],'sha256')
+			return new_user
 
 
 @api.route('/api/auth/login')
 class login(Resource):
-	@api.expect()
+	@api.expect(login_model)
 	def post(self):
-		pass
+		login_info=request.get_json()
+		if user_obj.email_verification(login_info['email']) == False:
+			return {'Error':'Incorrect Email Syntax'}
+		else:
+			return login_info
 		
 
 #user logout endpoint
 @api.route('/api/auth/logout')
 class logout(Resource):
-	@api.expect()
+	@api.expect(logout_model)
 	def post(self):
-		pass
+		logout_info=request.get_json()
+		if logout_info['confirm_logout'] == 'yes':
+			return {'Logged out' : True}
+		elif logout_info['confirm_logout'] == 'yes':
+			return {'log out':False}
+		else:
+			return {'Error':'invalid input'}
 
 
 #user password reset
 @api.route('/api/auth/reset-password')
 class passwordReset(Resource):
-	@api.expect()
+	@api.expect(password_reset)
 	def post(self):
-		pass
+		reset_info=request.get_json()
+		if user_obj.not_empty(reset_info['old_password'])== True:
+			return {'Error':'can not be empty'}
+		elif user_obj.not_empty(reset_info['new_password']) == True:
+			return {'Error':'can not be empty'}
+		elif user_obj.password_match(reset_info['new_password'], reset_info['confirm_password']) == False:
+			return {'Error':'passwords do not match'}
+		else:
+			return reset_info
 
 
 #register Business
--- api/test.py
+++ api/test.py
@@ -2,20 +2,30 @@
 import unittest
 from app import app
 from flask import json
+
+
 class registerTest(unittest.TestCase):
 	def setUp(self):
 		self.newInfo={
-		"firstName" : "kwame",
-		"lastName" : "asiago",
+		"first_name" : "kwame",
+		"last_name" : "asiago",
 		"email" : "kwame@gmail.com",
 		"password" : "1234",
-		"confirmPassword" : "1234"
+		"confirm_password" : "1234"
 		}
 		self.logInfo={
 		'email':'kwameasiago@gmail.com',
 		'password':'1234'
 		}
+		self.logout={'confirm_logout':'yes'}
+		self.password_reset={
+		'old_password':'1234',
+		'new_password':'asd',
+		'confirm_password':'asd'
+
+		}
 		self.header={'Content-type': 'application/json'}
+
 	def test_register_new(self):
 		test = app.test_client('self')
 		response = test.post('/api/auth/register' ,data=json.dumps(self.newInfo),headers=self.header)
@@ -25,7 +35,14 @@ class registerTest(unittest.TestCase):
 		test = app.test_client('self')
 		response = test.post('/api/auth/login',data=json.dumps(self.newInfo),headers=self.header)
 		self.assertTrue(response.status_code==200)
-
+	def test_logout(self):
+		test=app.test_client('self')
+		response = test.post('/api/auth/logout', data=json.dumps(self.logout),headers=self.header)
+		self.assertEqual(response.status_code,200)
+	def test_password_reset(self):
+		test=app.test_client('self')
+		response=test.post('/api/auth/reset-password',data=json.dumps(self.password_reset),headers=self.header)
+		self.assertEqual(response.status_code,200)
 
 
 
