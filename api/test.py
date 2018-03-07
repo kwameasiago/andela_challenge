7898ec488a32643cfc4a04213e2156e916bbb7f8
SelaDanti <kwameasiago@gmail.com>
2018-03-07 12:56:17 +0300

    add test for login endpoint

--- api/test.py
+++ api/test.py
@@ -4,22 +4,26 @@ from app import app
 from flask import json
 class registerTest(unittest.TestCase):
 	def setUp(self):
-		self.new={
+		self.newInfo={
 		"firstName" : "kwame",
 		"lastName" : "asiago",
 		"email" : "kwame@gmail.com",
 		"password" : "1234",
 		"confirmPassword" : "1234"
 		}
+		self.logInfo={
+		'email':'kwameasiago@gmail.com',
+		'password':'1234'
+		}
 		self.header={'Content-type': 'application/json'}
 	def test_register_new(self):
 		test = app.test_client('self')
-		response = test.post('/api/auth/register' ,data=json.dumps(self.new),headers=self.header)
+		response = test.post('/api/auth/register' ,data=json.dumps(self.newInfo),headers=self.header)
 		self.assertEqual(response.status_code, 200)
 		
-	def test_getmethod(self):
-		test = app.test_client(self)
-		response = test.get('/api/auth/register')
+	def test_login(self):
+		test = app.test_client('self')
+		response = test.post('/api/auth/login',data=json.dumps(self.newInfo),headers=self.header)
 		self.assertTrue(response.status_code==200)
 
 
